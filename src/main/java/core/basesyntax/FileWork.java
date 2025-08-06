eca8b8e27c6de7fe07005060900ce10e5221b325
Szymon Twardowski <szytwardowski@gmail.com>
2025-08-06 18:52:43 +0200

    words

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
-public class FileWork {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+
+
+class Words {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        List<String> result = new ArrayList<>();
+        List<String> lines = new ArrayList<>();
+
+        try {
+            lines = Files.readAllLines(Path.of(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String line : lines) {
+            String[] words = line.split("\\W+");
+            for (String word : words) {
+                if (!word.isEmpty() && word.toLowerCase().startsWith("w")){
+                    result.add(word.toLowerCase());
+                }
+            }
+        }
+        Collections.sort(result);
+        return result.toArray(new String[0]);
     }
 }
