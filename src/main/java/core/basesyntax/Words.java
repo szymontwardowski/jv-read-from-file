34cf9dbb55ada3774dbacd2f79437ec3380935ae
Szymon Twardowski <szytwardowski@gmail.com>
2025-08-06 19:00:20 +0200

    words#2

--- src/main/java/core/basesyntax/Words.java
+++ src/main/java/core/basesyntax/Words.java
@@ -7,21 +7,21 @@ import java.util.List;
 import java.util.Collections;
 import java.util.ArrayList;
 
-
 class Words {
     public String[] readFromFile(String fileName) {
-        List<String> result = new ArrayList<>();
-        List<String> lines = new ArrayList<>();
-
+        List<String> lines;
         try {
             lines = Files.readAllLines(Path.of(fileName));
         } catch (IOException e) {
-            e.printStackTrace();
+            return new String[0];
         }
+
+        List<String> result = new ArrayList<>();
+
         for (String line : lines) {
             String[] words = line.split("\\W+");
             for (String word : words) {
-                if (!word.isEmpty() && word.toLowerCase().startsWith("w")){
+                if (!word.isEmpty() && word.toLowerCase().startsWith("w")) {
                     result.add(word.toLowerCase());
                 }
             }
